8eacb70ead8907ea4d15134354938d54e1429fa2
Notos <lianggh159@outlook.com>
2026-02-13 13:09:53 +0800

    add code comments in routes.ts

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,7 +1,10 @@
 import type { RouteRecordRaw } from 'vue-router'
+// import { foo } from './foo'
+// import { bar } from './bar'
 
 const routes: RouteRecordRaw[] = [
-
+//   foo,
+//   bar,
 ]
 
 export default routes
